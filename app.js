8adf0e334434bda28648cde141bd14435a3035f6
unknown <Esohe.dev web 3 assignment>
2025-12-10 12:23:33 +0100

    corrections made on index.html

---
+++ app.js
@@ -0,0 +1,8 @@
+addEventListener("load", function changePage() {
+  setTimeout(() => {
+    window.location.href = "register.html";
+  }, 3000);
+});
+
+// changing page - c
+// Collect user details to signup or login
--- login.js
+++ login.js
@@ -17,7 +17,7 @@ if (loginbtn) {
    .then(() => {
     // Signed in 
     // Redirect to dashbord or where ever you want it to go
-      window.location.href = "index.html";
+      window.location.href = "home.html";
   })
   .catch((error) => {
     const errorCode = error.code;
